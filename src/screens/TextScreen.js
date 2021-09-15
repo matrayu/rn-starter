490abe9ca8e13717e07fc349cbcdc5943e70cceb
Matthew Friedberg <mfriedberg@netflix.com>
2021-09-14 18:06:55 -0700

    Added state management and password verification

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,25 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, StyleSheet, View, TextInput } from "react-native";
 
 const TextScreen = () => {
-    return (
-        <View>
-            <TextInput 
-                style={styles.input}
-                autoCapitalize="none"
-                autoCorrect={false}
-            />
-        </View>
-    );
+  const [password, setPassword] = useState("");
+
+  return (
+    <View>
+      <Text>Enter Password</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={password}
+        onChangeText={(newValue) => setPassword(newValue)}
+      />
+      {password.length < 6 ? <Text>Password must be more than 5 characters</Text> : null}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    input: {
-        margin: 15,
-        borderColor: 'black',
-        borderWidth: 1,
-        height: 30,
-    }
+  input: {
+    margin: 15,
+    borderColor: "black",
+    borderWidth: 1,
+    height: 30,
+  },
 });
 
 export default TextScreen;
