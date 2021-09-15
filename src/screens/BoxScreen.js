6202db3dc0848d490880aa59a00ef8bbd853eda6
Matthew Friedberg <mfriedberg@netflix.com>
2021-09-14 23:07:32 -0700

    Finished with tutorial on basic layout in RN

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,12 +3,58 @@ import { Text, StyleSheet, View } from "react-native";
 
 const BoxScreen = () => {
     return (
-        <View>
-            <Text>Box Screen</Text>
+        <View style={styles.viewStyle}>
+            <View style={styles.viewOneStyle}></View>
+            <View style={styles.viewTwoStyle}></View>
+            <View style={styles.viewThreeStyle}></View>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 1,
+        borderColor: 'black',
+        height: 300,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        //alignItems: "",
+        
+    },
+    viewOneStyle: {
+        height: 100,
+        width: 100,
+        backgroundColor: 'red',
+        borderWidth: 3,
+        borderColor: 'red',
+        margin: 5,
+        padding: 5,
+    },
+    viewTwoStyle: {
+        height: 100,
+        width: 100,
+        backgroundColor: 'green',
+        borderWidth: 3,
+        borderColor: 'green',
+        margin: 5,
+        padding: 5,
+        alignSelf: "flex-end",
+        //marginTop: 105,
+        //position: 'absolute',
+        //left: 145,
+        //top: 105,
+        //...StyleSheet.absoluteFillObject
+
+    },
+    viewThreeStyle: {
+        height: 100,
+        width: 100,
+        borderWidth: 3,
+        backgroundColor: 'blue',
+        borderColor: 'blue',
+        margin: 5,
+        padding: 5,
+    }
+});
 
 export default BoxScreen;
